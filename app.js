d8468510c522480dd76748a8662c4529627de417
Tony Levis <tonylevis@TonydeMacBook-Air.local>
2019-09-08 09:40:55 +0800

    add function

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-let express = require('express');
-let wechat = require('./wechat/wechat');
-let config ={
-  token:'test'
+const express = require('express');
+const wechat = require('./wechat/wechat');
+var config ={
+  token:'test',
+  appID:'wxabc82c1261260125',
+  appsecret:'facef4ec57a5965b24fb3a6a732e4a2e',
+  apiDomain:'https://api.weixin.qq.com',
+  accessTokenApi:"%scgi-bin/token?grant_type=client_credential&appid=%s&secret=%s"
 };
 
 let app = express();
 let wecharApp = new wechat(config);
 app.get('/' ,function(req,res){
+   console.log("listening");
    wecharApp.auth(req,res);
+});
+
+app.get('/getAccessToken',function(req,res){
+  wecharApp.getAccessToken().then(function(data){
+    res.send(data);
+  });
 });
 
 app.listen(3000);
--- wechat/wechat.js
+++ wechat/wechat.js
@@ -4,6 +4,27 @@ let crypto = require('crypto');
 var weChat = function(config){
     this.config = config;
     this.token = config.token;
+    this.appID = config.appID;
+    this.appScrect = config.appScrect;
+    this.apiDomain = config.apiDomain;
+    this.accessTokenApi = config.accessTokenApi;
+
+    this.requestGet = function(url){
+        return new Promise(function(resolve,reject){
+            https.get(url,function(res){
+                var buffer = [],result = "";
+                res.on('data',function(data){
+                    buffer.push(data);
+                });
+                res.on('end',function(){
+                    result = Buffer.concat(buffer,buffer.length).toString('utf-8');
+                    resolve(result);
+                });
+            }).on('error',function(err){
+                reject(err);
+            });
+        });
+    }
 }
 
 weChat.prototype.auth = function(req,res){
@@ -26,5 +47,27 @@ weChat.prototype.auth = function(req,res){
   }
 }
 
+weChat.prototype.getAccessToken = function(){
+    var that = this;
+    return new Promise(function(resolve,reject){
+        var currentTime = new Date().getTime();
+        var url = util.format(that.apiURL.accessTokenApi,that.apiDomain,that.appID,that.appScrect);
+        if(accessTokenJson.access_token === "" || accessTokenJson.expires_time < currentTime){
+            that.requestGet(url).then(function(data){
+                var result = JSON.parse(data); 
+                if(data.indexOf("errcode") < 0){
+                    accessTokenJson.access_token = result.access_token;
+                    accessTokenJson.expires_time = new Date().getTime() + (parseInt(result.expires_in) - 200) * 1000;
+                    fs.writeFile('./wechat/access_token.json',JSON.stringify(accessTokenJson));
+                    resolve(accessTokenJson.access_token);
+                }else{
+                    reject(result);
+                } 
+            });
+        }else{
+            resolve(accessTokenJson.access_token);  
+        }
+    });
+}
 
 module.exports = weChat;
